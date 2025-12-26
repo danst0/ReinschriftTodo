fe5a7618dd3ff48824a1fa1cc55e2feda547d5e7
Daniel <daniel@seneca>
2025-12-26 21:02:12 +0100

    Update Flatpak build for Whisper support: added LLVM/ALSA dependencies and updated vendored sources

--- build_flatpak.sh
+++ build_flatpak.sh
@@ -4,7 +4,7 @@ set -e
 # Install dependencies (if not already installed)
 echo "Ensuring Runtime and SDK are installed..."
 # We use --or-update to ensure we have the latest version
-flatpak install --user --noninteractive --or-update org.gnome.Platform//49 org.gnome.Sdk//49 org.freedesktop.Sdk.Extension.rust-stable//25.08
+flatpak install --user --noninteractive --or-update org.gnome.Platform//49 org.gnome.Sdk//49 org.freedesktop.Sdk.Extension.rust-stable//25.08 org.freedesktop.Sdk.Extension.llvm18//25.08
 
 # Build the Flatpak
 echo "Building Flatpak..."
